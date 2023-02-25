29620508a56ee35e17c0463a002356e000fddf88
Nantadechx <api.nantadech@gmail.com>
2023-02-25 17:06:54 +0700

    dev

--- index.js
+++ index.js
@@ -27,7 +27,5 @@ app.get('/api/provinces', checkSecretKey, (req, res) => {
   res.json(data);
 });
 
-app.listen(10000, () => {
-  console.log('API server listening on http://localhost:10000');
-});
+app.listen(process.env.PORT || 3000)
 module.exports = app;
